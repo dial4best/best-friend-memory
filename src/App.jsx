c5ee7ae833fa84f80673a109f7ca5e0f7d98229a
Dhruvita Purohit <dhupurohit@gmail.com>
2026-01-30 20:39:22 +0530

    Base project setup done with tailwid css

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="min-h-screen flex items-center justify-center px-6">
+      <h1 className="text-center text-3xl md:text-5xl leading-relaxed">
+        This page is being built <br />
+        <span className="italic text-xl md:text-2xl block mt-4">
+          for someone who matters.
+        </span>
+      </h1>
+    </main>
   )
 }
-
-export default App
